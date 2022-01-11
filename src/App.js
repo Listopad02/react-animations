d13fe040fa63797b031c967fb9e5b78638f5c83d
Listopad02 <123ayetvbass@gmail.com>
2022-01-11 20:24:15 +0300

    animation for a group of elements added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { useState } from "react"
 import { Transition, CSSTransition } from 'react-transition-group';
+import { List } from "./List";
 
 function App() {
   const [toggle, setToggle] = useState(true)
   const [toggle2, setToggle2] = useState(true)
+  const [items, setItems] = useState([
+    {id: 1, title: 'Item 1'},
+    {id: 2, title: 'Item 2'},
+    {id: 3, title: 'Item 3'}
+  ])
+
+  const removeItem = id => setItems(items.filter(i => i.id !== id))
+
+  const addItem = () => {
+    const title = prompt('Enter the title!')
+    const id = Date.now()
+
+    setItems(items.concat([{title, id}]))
+  }
 
   return (
     <div className="container">
       <button onClick={() => setToggle(!toggle)}>Toggle first</button>
       <button onClick={() => setToggle2(!toggle2)}>Toggle second</button>
+      <button onClick={addItem}>Add item</button>
       <hr />
       <div className="blocks">
         <Transition in={toggle}
@@ -33,6 +49,9 @@ function App() {
                        unmountOnExit>
           <div className="square orange">{toggle2.toString()}</div>
         </CSSTransition>
+      </div>
+      <div className="blocks">
+        <List items={items} onRemove={removeItem} />
       </div>
     </div>
   );
